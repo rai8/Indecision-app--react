70fb98aaf59edae278821936266743703448ccea
rai8 <hytonne@outlook.com>
2019-08-12 17:11:03 +0300

    dynamic rendering of jsx

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -2,39 +2,72 @@
 
 console.log("App js is running smoothly");
 //JSX - Javascript XML
-/* var template = (
-  <div>
-    {" "}
-    <h1>Indecision app</h1>
-    <p>Here, is some more information</p>
-    <ol>
-      <li>Item one</li>
-      <li>Item two</li>
-    </ol>
-  </div>
+var titleApp = {
+  title: "Indecision App",
+  subtitle: "Here, you will find more information",
+  itemLIst: ["Item one", "Item two"]
+};
+var template = React.createElement(
+  "div",
+  null,
+  " ",
+  React.createElement(
+    "h1",
+    null,
+    titleApp.title
+  ),
+  React.createElement(
+    "p",
+    null,
+    titleApp.subtitle
+  ),
+  React.createElement(
+    "ol",
+    null,
+    React.createElement(
+      "li",
+      null,
+      titleApp.itemLIst[0]
+    ),
+    React.createElement(
+      "li",
+      null,
+      titleApp.itemLIst[1]
+    )
+  )
 );
 
 var appRoot = document.getElementById("root");
 ReactDOM.render(template, appRoot);
- */
+var user = {
+  name: "Bridgit Akinyi",
+  age: 16,
+  location: "Kasarani, Nairobi"
+};
+/* let userName = "Rhytonne Odhiambo";
+let userAge = 21;
+let userLocation = "Mombasa, Kenya"; */
 var template2 = React.createElement(
   "div",
   null,
   React.createElement(
     "h1",
     null,
-    "Rhytonne Odhiambo"
+    user.name.toUpperCase()
   ),
   React.createElement(
     "p",
     null,
-    "Age:21 "
+    "Age:",
+    user.age,
+    " "
   ),
   React.createElement(
     "p",
     null,
-    "Location:Mombasa, Kenya"
+    "Location: ",
+    user.location
   )
 );
-var indexRoot = document.getElementById("root");
-ReactDOM.render(template2, indexRoot);
+//let indexRoot = document.getElementById("root");
+//ReactDOM.render(template2, indexRoot);
--- src/app.js
+++ src/app.js
@@ -1,26 +1,38 @@
 console.log("App js is running smoothly");
 //JSX - Javascript XML
-/* var template = (
+let titleApp = {
+  title: "Indecision App",
+  subtitle: "Here, you will find more information",
+  itemLIst: ["Item one", "Item two"]
+};
+var template = (
   <div>
     {" "}
-    <h1>Indecision app</h1>
-    <p>Here, is some more information</p>
+    <h1>{titleApp.title}</h1>
+    <p>{titleApp.subtitle}</p>
     <ol>
-      <li>Item one</li>
-      <li>Item two</li>
+      <li>{titleApp.itemLIst[0]}</li>
+      <li>{titleApp.itemLIst[1]}</li>
     </ol>
   </div>
 );
 
 var appRoot = document.getElementById("root");
 ReactDOM.render(template, appRoot);
- */
+let user = {
+  name: "Bridgit Akinyi",
+  age: 16,
+  location: "Kasarani, Nairobi"
+};
+/* let userName = "Rhytonne Odhiambo";
+let userAge = 21;
+let userLocation = "Mombasa, Kenya"; */
 let template2 = (
   <div>
-    <h1>Rhytonne Odhiambo</h1>
-    <p>Age:21 </p>
-    <p>Location:Mombasa, Kenya</p>
+    <h1>{user.name.toUpperCase()}</h1>
+    <p>Age:{user.age} </p>
+    <p>Location: {user.location}</p>
   </div>
 );
-let indexRoot = document.getElementById("root");
-ReactDOM.render(template2, indexRoot);
+//let indexRoot = document.getElementById("root");
+//ReactDOM.render(template2, indexRoot);
