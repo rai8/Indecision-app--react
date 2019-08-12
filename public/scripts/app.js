908471a641ea3d11c3c3d3e7f413375c0f90cb4c
rai8 <hytonne@outlook.com>
2019-08-12 16:08:04 +0300

    babel setup and dependancies configuration

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,10 +1,12 @@
+"use strict";
+
 console.log("App js is running smoothly");
 //JSX - Javascript XML
-/* var template = <p>This is our first JSX from app.js</p>; */
 var template = React.createElement(
   "h1",
   null,
-  "This is our first JSX from app.js"
+  "Indecision app"
 );
+
 var appRoot = document.getElementById("root");
 ReactDOM.render(template, appRoot);
---
+++ src/app.js
@@ -0,0 +1,6 @@
+console.log("App js is running smoothly");
+//JSX - Javascript XML
+var template = <h1>Indecision app</h1>;
+
+var appRoot = document.getElementById("root");
+ReactDOM.render(template, appRoot);
