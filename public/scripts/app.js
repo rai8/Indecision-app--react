c918cdbf102ba230f8812c89f05008aeadfddbcb
rai8 <hytonne@outlook.com>
2019-08-12 16:43:41 +0300

    jsx expressions

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -2,11 +2,39 @@
 
 console.log("App js is running smoothly");
 //JSX - Javascript XML
-var template = React.createElement(
-  "h1",
-  null,
-  "Indecision app"
+/* var template = (
+  <div>
+    {" "}
+    <h1>Indecision app</h1>
+    <p>Here, is some more information</p>
+    <ol>
+      <li>Item one</li>
+      <li>Item two</li>
+    </ol>
+  </div>
 );
 
 var appRoot = document.getElementById("root");
 ReactDOM.render(template, appRoot);
+ */
+var template2 = React.createElement(
+  "div",
+  null,
+  React.createElement(
+    "h1",
+    null,
+    "Rhytonne Odhiambo"
+  ),
+  React.createElement(
+    "p",
+    null,
+    "Age:21 "
+  ),
+  React.createElement(
+    "p",
+    null,
+    "Location:Mombasa, Kenya"
+  )
+);
+var indexRoot = document.getElementById("root");
+ReactDOM.render(template2, indexRoot);
--- src/app.js
+++ src/app.js
@@ -1,6 +1,26 @@
 console.log("App js is running smoothly");
 //JSX - Javascript XML
-var template = <h1>Indecision app</h1>;
+/* var template = (
+  <div>
+    {" "}
+    <h1>Indecision app</h1>
+    <p>Here, is some more information</p>
+    <ol>
+      <li>Item one</li>
+      <li>Item two</li>
+    </ol>
+  </div>
+);
 
 var appRoot = document.getElementById("root");
 ReactDOM.render(template, appRoot);
+ */
+let template2 = (
+  <div>
+    <h1>Rhytonne Odhiambo</h1>
+    <p>Age:21 </p>
+    <p>Location:Mombasa, Kenya</p>
+  </div>
+);
+let indexRoot = document.getElementById("root");
+ReactDOM.render(template2, indexRoot);
